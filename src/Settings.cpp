fee5ba447534285af1cef5624245ec6f299494cc
powerof3 <powerof3@users.noreply.github.com>
2024-10-21 11:55:52 +0530

    Update settings

--- src/Settings.cpp
+++ src/Settings.cpp
@@ -25,26 +25,26 @@ namespace Splashes
 
 	void Projectile::LoadSettings(CSimpleIniA& a_ini, bool a_writeComment)
 	{
-		ini::get_value(a_ini, enableSplash, type.c_str(), "Enable Water Splashes", a_writeComment ? ";Enable water splashes (impact effects)" : nullptr);
-		ini::get_value(a_ini, enableRipple, type.c_str(), "Enable Water Ripples", a_writeComment ? ";Enable water ripples" : nullptr);
+		ini::get_value(a_ini, enableSplash, type.c_str(), "bWaterSplashes", a_writeComment ? ";Enable water splashes (impact effects)." : nullptr);
+		ini::get_value(a_ini, enableRipple, type.c_str(), "bWaterRipples", a_writeComment ? ";Enable water ripples." : nullptr);
 
-		ini::get_value(a_ini, displacementMult, type.c_str(), "RippleDisplacementMult", a_writeComment ? ";Ripple displacement multiplier. Higher values = more elongated ripples" : nullptr);
-		ini::get_value(a_ini, modelPath, type.c_str(), "Nif Path", a_writeComment ? ";Path is relative to Data directory" : nullptr);
-		ini::get_value(a_ini, modelPathFire, type.c_str(), "Nif Path [Fire]", nullptr);
-		ini::get_value(a_ini, modelPathDragon, type.c_str(), "Nif Path [Dragon]", nullptr);
+		ini::get_value(a_ini, displacementMult, type.c_str(), "fRippleDisplacementMult", a_writeComment ? ";Ripple displacement multiplier. Higher values = more elongated ripples." : nullptr);
+		ini::get_value(a_ini, modelPath, type.c_str(), "sNifPath", a_writeComment ? ";Path is relative to Data directory." : nullptr);
+		ini::get_value(a_ini, modelPathFire, type.c_str(), "sNifPathFire", nullptr);
+		ini::get_value(a_ini, modelPathDragon, type.c_str(), "sNifPathDragonFire", nullptr);
 	}
 
 	void Explosion::LoadSettings(CSimpleIniA& a_ini)
 	{
-		ini::get_value(a_ini, enable, type.c_str(), "Enable", ";Enable underwater explosions");
-		ini::get_value(a_ini, fireOnly, type.c_str(), "Fire Explosions Only", ";Only fire based explosions will trigger underwater explosions");
+		ini::get_value(a_ini, enable, type.c_str(), "bEnable", nullptr);
+		ini::get_value(a_ini, fireOnly, type.c_str(), "bFireExplosionsOnly", nullptr);
 
-		ini::get_value(a_ini, displacementMult, type.c_str(), "RippleDisplacementMult", nullptr);
-		ini::get_value(a_ini, modelPath, type.c_str(), "Nif Path", nullptr);
-		ini::get_value(a_ini, modelPathFire, type.c_str(), "Nif Path [Fire]", nullptr);
-		ini::get_value(a_ini, modelPathDragon, type.c_str(), "Nif Path [Dragon]", nullptr);
+		ini::get_value(a_ini, displacementMult, type.c_str(), "fRippleDisplacementMult", nullptr);
+		ini::get_value(a_ini, modelPath, type.c_str(), "sNifPath", nullptr);
+		ini::get_value(a_ini, modelPathFire, type.c_str(), "sNifPathFire", nullptr);
+		ini::get_value(a_ini, modelPathDragon, type.c_str(), "sNifPathDragonFire", nullptr);
 
-		ini::get_value(a_ini, splashRadius, type.c_str(), "Default Explosion Splash Radius", ";Default explosion radius (for scaling underwater explosions)");
+		ini::get_value(a_ini, splashRadius, type.c_str(), "fDefaultExplosionSplashRadius", nullptr);
 	}
 
 	void Settings::LoadSettings()
@@ -56,16 +56,16 @@ namespace Splashes
 
 		ini.LoadFile(path);
 
-		ini::get_value(ini, patchDisplacement, "Settings", "Enable Water Displacement", ";Enables water displacement on all water surfaces");
-		ini::get_value(ini, allowDamageWater, "Settings", "Enable Dangerous Water", ";Enables splashes on water marked as dangerous (i.e survival mods use this).\n;Lava and other non water surfaces may trigger water splashes!");
+		ini::get_value(ini, patchDisplacement, "Settings", "bWaterDisplacement", ";Enables water displacement on all water surfaces.");
+		ini::get_value(ini, allowDamageWater, "Settings", "bSplashesOnDangerousWater", ";Enables splashes on water marked as dangerous (i.e survival mods use this).\n;Lava and other non water surfaces may trigger water splashes.");
 
-		ini::get_value(ini, splashRadii[SIZE::kHeavy], "Settings", "Projectile Size [Heavy]", ";Size of the projectile that will trigger splashes");
-		ini::get_value(ini, splashRadii[SIZE::kMedium], "Settings", "Projectile Size [Medium]", nullptr);
-		ini::get_value(ini, splashRadii[SIZE::kLight], "Settings", "Projectile Size [Light]", nullptr);
+		ini::get_value(ini, splashRadii[SIZE::kHeavy], "Settings", "fProjectileSizeHeavy", ";Size of the projectile that will trigger splashes.");
+		ini::get_value(ini, splashRadii[SIZE::kMedium], "Settings", "fProjectileSizeMedium", nullptr);
+		ini::get_value(ini, splashRadii[SIZE::kLight], "Settings", "fProjectileSizeLight", nullptr);
 
-		ini::get_value(ini, splashScales[SIZE::kHeavy], "Settings", "Splash Effect Scale [Heavy]", ";Splash effect scale");
-		ini::get_value(ini, splashScales[SIZE::kMedium], "Settings", "Splash Effect Scale [Medium]", nullptr);
-		ini::get_value(ini, splashScales[SIZE::kLight], "Settings", "Splash Effect Scale [Light]", nullptr);
+		ini::get_value(ini, splashScales[SIZE::kHeavy], "Settings", "fSplashEffectScaleHeavy", ";Splash effect scale.");
+		ini::get_value(ini, splashScales[SIZE::kMedium], "Settings", "fSplashEffectScaleMedium", nullptr);
+		ini::get_value(ini, splashScales[SIZE::kLight], "Settings", "fSplashEffectScaleLight", nullptr);
 
 		missile.LoadSettings(ini, true);
 		flame.LoadSettings(ini);
